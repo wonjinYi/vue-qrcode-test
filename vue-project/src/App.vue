68f16800d6c9c9fea681b4b1df5ea8c75a84bf8d
wonjinYi <studioplug17@gmail.com>
2023-03-23 16:55:10 +0900

    야호야호

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  <main>
+    <img :src="qrcodeImg" />
+  </main>
+
+  <div class="stream">
+    <qr-stream @decode="onDecode" class="mb">
+      <div style="color: red;" class="frame"></div>
+    </qr-stream>
+  </div>
+  <div class="result">
+    Result: {{state.data}}
+  </div>
+</template>
+
+<script setup>
+import qrcodeImg from './assets/qrcode.png'
+import { QrStream, QrCapture, QrDropzone } from 'vue3-qr-reader';
+import { reactive } from 'vue';
+const state = reactive({
+  data: null
+})
+function onDecode(data) {
+  console.log(data);
+  state.data = data
+}
+</script>
